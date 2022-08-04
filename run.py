572ad0eb6611874359a9de37d9e7d54b85febc36
Kijusu <75220799+MaxenceR26@users.noreply.github.com>
2022-08-04 23:04:55 +0200

    Patch error

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 from app.main import app
 
 if __name__ == "__main__":
     app.run()
-=======
-from app.main import app
-
-if __name__ == "__main__":
-    app.run()
->>>>>>> 83949628b833b40f402313b309c770639ef05f91
